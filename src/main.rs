b0b075a12cd7efc85a2445d875a499d8c489404b
w <w@localhost>
2024-05-09 17:36:34 +0100

    rename pact -> pacmon

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-mod pact;
+mod pacmon;
 mod etc;
 mod ui;
 mod subnets;
@@ -11,6 +11,6 @@ mod pcap;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    pact::run(args);
+    pacmon::run(args);
 }
 
