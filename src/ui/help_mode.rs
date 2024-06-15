06ea7b3405ad6d7ce88345ea59f3520345c8e97d
w <w@localhost>
2024-06-15 11:52:12 +0100

    introducing help_mode

---
+++ src/ui/help_mode.rs
@@ -0,0 +1,55 @@
+use std::sync::atomic::Ordering::Relaxed;
+use chrono::Local;
+use ncurses::{ACS_HLINE, ACS_LLCORNER, ACS_LRCORNER, ACS_ULCORNER, ACS_URCORNER, ACS_VLINE, clear, COLS, LINES, mvaddch, mvhline, mvprintw, mvvline, refresh};
+use crate::etc::fmt_millis;
+use crate::pacstream::PacStream;
+use crate::ui::{CMD_INFO, PAUSED, RESOLVE, speed};
+
+pub(crate) fn print(pac_vec: &Vec<PacStream>, widths: Vec<i16>, q_depth: u64, dropped: u64, interval: u64, last_draw: i64) {
+    clear();
+
+    mvaddch(0, 0, ACS_ULCORNER());
+    mvhline(0, 1, ACS_HLINE(), COLS() - 2);
+    mvaddch(0, COLS() - 1, ACS_URCORNER());
+    mvvline(1, 0, ACS_VLINE(), LINES() - 2);
+    mvvline(1, COLS() - 1, ACS_VLINE(), LINES() - 2);
+    mvaddch(LINES() - 1, 0, ACS_LLCORNER());
+    mvhline(LINES() - 1, 1, ACS_HLINE(), COLS() - 2);
+    mvaddch(LINES() - 1, COLS() - 1, ACS_LRCORNER());
+
+    let bytes_sent_last: u64 = pac_vec.iter().map(|s| s.bytes_sent_last).sum();
+    let bytes_recv_last: u64 = pac_vec.iter().map(|s| s.bytes_recv_last).sum();
+    let resolve = RESOLVE.fetch_and(true, Relaxed);
+    let pause = PAUSED.fetch_and(true, Relaxed);
+
+    let mut tt = vec![
+        format!("     q depth: {:<8} pacs drop'd: {}", q_depth, dropped),
+        format!("     resolve: {:<8} pause: {:?}", resolve.to_string(), pause),
+        format!("   last_draw: {}", fmt_millis(last_draw)),
+        format!("        recv: {:<8} sent:{:<8} interval: {:?}",
+                speed(bytes_recv_last, interval), speed(bytes_sent_last, interval), interval),
+        format!("      widths: {:?}", widths),
+        format!("    commands: {}", CMD_INFO.lock().unwrap().iter()
+            .map(|(c, txt)| format!("'{}':{}", c, txt))
+            .collect::<Vec<String>>()
+            .join("  ")
+        )
+    ];
+
+    for t in &mut tt {
+        t.truncate(COLS() as usize - 2);
+    }
+
+    let width = tt.iter().max_by_key(|s| s.len()).unwrap().len();
+    let x_offset = (COLS() - width as i32) / 2;
+    let y_offset = (LINES() - tt.len() as i32) / 2;
+
+    for i in 0..tt.len() {
+        mvprintw(i as i32 + y_offset, x_offset, &tt[i]);
+    }
+
+    mvprintw(LINES() - 1, COLS() - 19, &format!("{:?}", Local::now().time()));
+
+    refresh();
+}
+
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,5 +1,6 @@
 mod corp_mode;
 mod normal_mode;
+mod help_mode;
 
 use std::{panic, sync, thread};
 use std::backtrace::Backtrace;
@@ -13,7 +14,7 @@ use chrono::{Local};
 use ncurses::*;
 use once_cell::sync::Lazy;
 
-use crate::etc::{fmt_millis, log, mag_fmt, millitime};
+use crate::etc::{log, mag_fmt, millitime};
 use crate::pacdat::StreamKey;
 use crate::pacstream::PacStream;
 use crate::ui::Justify::{LHS, RHS};
@@ -125,7 +126,7 @@ impl UI {
         let interval = (now - self.last_draw) as u64;
 
         if HELP.fetch_and(true, Relaxed) {
-            self.print_help(&pac_vec, prev_widths, q_depth, dropped, interval);
+            help_mode::print(&pac_vec, prev_widths, q_depth, dropped, interval, self.last_draw);
         } else {
             if CORPORATE_MODE.fetch_and(true, Relaxed) {
                 corp_mode::print(&pac_vec, prev_widths, q_depth, dropped, interval);
@@ -137,55 +138,6 @@ impl UI {
 
         self.last_draw = now;
     }
-
-    fn print_help(&self, pac_vec: &Vec<PacStream>, widths: Vec<i16>, q_depth: u64, dropped: u64, interval: u64) {
-        clear();
-
-        mvaddch(0, 0, ACS_ULCORNER());
-        mvhline(0, 1, ACS_HLINE(), COLS() - 2);
-        mvaddch(0, COLS() - 1, ACS_URCORNER());
-        mvvline(1, 0, ACS_VLINE(), LINES() - 2);
-        mvvline(1, COLS() - 1, ACS_VLINE(), LINES() - 2);
-        mvaddch(LINES() - 1, 0, ACS_LLCORNER());
-        mvhline(LINES() - 1, 1, ACS_HLINE(), COLS() - 2);
-        mvaddch(LINES() - 1, COLS() - 1, ACS_LRCORNER());
-
-        let bytes_sent_last: u64 = pac_vec.iter().map(|s| s.bytes_sent_last).sum();
-        let bytes_recv_last: u64 = pac_vec.iter().map(|s| s.bytes_recv_last).sum();
-        let resolve = RESOLVE.fetch_and(true, Relaxed);
-        let pause = PAUSED.fetch_and(true, Relaxed);
-
-        let mut tt = vec![
-            format!("     q depth: {:<8} pacs drop'd: {}", q_depth, dropped),
-            format!("     resolve: {:<8} pause: {:?}", resolve.to_string(), pause),
-            format!("   last_draw: {}", fmt_millis(self.last_draw)),
-            format!("        recv: {:<8} sent:{:<8} interval: {:?}",
-                    speed(bytes_recv_last, interval), speed(bytes_sent_last, interval), interval),
-            format!("      widths: {:?}", widths),
-            format!("    commands: {}", CMD_INFO.lock().unwrap().iter()
-                .map(|(c, txt)| format!("'{}':{}", c, txt))
-                .collect::<Vec<String>>()
-                .join("  ")
-            )
-        ];
-
-        for t in &mut tt {
-            t.truncate(COLS() as usize - 2);
-        }
-
-        let width = tt.iter().max_by_key(|s| s.len()).unwrap().len();
-        let x_offset = (COLS() - width as i32) / 2;
-        let y_offset = (LINES() - tt.len() as i32) / 2;
-
-        for i in 0..tt.len() {
-            mvprintw(i as i32 + y_offset, x_offset, &tt[i]);
-        }
-
-        mvprintw(LINES() - 1, COLS() - 19, &format!("{:?}", Local::now().time()));
-
-        refresh();
-    }
-
 
     fn print_footer(q_depth: u64, dropped: u64) {
         let footer = Self::render_footer(q_depth, dropped);
